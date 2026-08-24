fda5fc37ae63680715e9bc42b6b58725ab2c883d
janhavishukla-1509 <janhavishukla249@gmail.com>
2026-08-24 19:21:34 +0530

    Time: 4 ms (41.50%) | Memory: 55 MB (86.21%) - LeetSync

---
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        // int mini = 1;
+        // for(int i = 0; i < n; i ++){
+        //     if(nums[i] < 0) continue;
+        //     if(nums[i] == mini){
+        //         mini ++;
+        //         i = 0;
+        //         continue;
+        //     }
+        //     else{
+        //         continue;
+        //     }
+
+        // }
+        for(int i =0 ; i < n; i++){
+            while(nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]){
+                swap(nums[i], nums[nums[i] - 1]);
+
+            }
+        }
+            for(int i = 0; i < n; i ++){
+                if(nums[i] != i + 1){
+                    return i + 1;
+                }
+            }
+            return n + 1;
+    }
+};
