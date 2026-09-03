65d9de898b5fefd4edd01af96d4f69b5c244e87a
janhavishukla-1509 <janhavishukla249@gmail.com>
2026-09-03 15:17:11 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (19.62%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -4,8 +4,8 @@ public:
         int idx = 0;
         for(int i = 0; i < nums.size(); i ++){
             if(nums[i] != 0){
-                swap(nums[idx], nums[i]);
-                idx ++;
+                swap(nums[idx++], nums[i]);
+                
             }
         }
     }
